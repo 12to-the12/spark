21f4b99bb0aa58705e140beee2447dd6d8f88855
Logan Hillyer <shivastranded@proton.me>
2023-07-19 14:15:13 +0700

    basic script in place

--- spark.sh
+++ spark.sh
@@ -1,1 +1,28 @@
 echo "And we see a spark of life"
+
+echo "Please refer to"
+echo "https://asus-linux.org/wiki/fedora-guide/"
+
+sudo dnf update -y
+
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+
+sudo dnf update -y
+sudo dnf install kernel-devel
+sudo dnf install akmod-nvidia xorg-x11-drv-nvidia-cuda
+
+sudo systemctl enable nvidia-hibernate.service nvidia-suspend.service nvidia-resume.service nvidia-powerd.service
+
+
+sudo dnf copr enable lukenukem/asus-linux
+sudo dnf update
+
+
+sudo dnf install asusctl supergfxctl
+sudo dnf update --refresh
+sudo systemctl enable supergfxd.service
+
+sudo dnf install asusctl-rog-gui
+
+echo "Then reboot"
