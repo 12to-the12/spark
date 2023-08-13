46a29695022ba6518c89991e8fb8b092be177824
Logan Hillyer <shivastranded@proton.me>
2023-08-12 20:36:55 -0700

    idk

--- spark.sh
+++ spark.sh
@@ -5,32 +5,48 @@ echo "https://asus-linux.org/wiki/fedora-guide/"
 echo "for the G-14"
 sudo dnf update -y
 
+
+# for ssh
 sudo dnf install openssh-server
 sudo systemctl enable sshd
 sudo systemctl start sshd
 sudo systemctl status sshd
 
-
+# additional repo
 sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-
-
 sudo dnf update -y
+
+# for nvidia drivers
 sudo dnf install kernel-devel
 sudo dnf install akmod-nvidia xorg-x11-drv-nvidia-cuda
 
 sudo systemctl enable nvidia-hibernate.service nvidia-suspend.service nvidia-resume.service nvidia-powerd.service
 
-
+# for asus stuff
 sudo dnf copr enable lukenukem/asus-linux
 sudo dnf update
-
 
 sudo dnf install asusctl supergfxctl
 sudo dnf update --refresh
 sudo systemctl enable supergfxd.service
-
 sudo dnf install asusctl-rog-gui
 
 
+# blender
 
-echo "Then reboot"
+flatpak install flathub org.blender.Blender
+
+
+# qbittorent
+cd ~/Downloads
+wget https://www.fosshub.com/qBittorrent.html?dwl=qbittorrent-4.5.4_x86_64.AppImage
+
+# qbittorent cli
+sudo rpm --import 'https://dl.cloudsmith.io/public/qbittorrent-cli/qbittorrent-cli/gpg.F8756541ADDA2B7D.key'
+sudo dnf config-manager --add-repo https://repos.fedarovich.com/fedora/37/qbittorrent-cli.repo
+
+sudo dnf install qbittorrent-cli
+
+
+
+echo "please reboot"
