6c2e590e7f0ad52b5cb0ccb7342f52260edf6008
Logan Hillyer <shivastranded@proton.me>
2023-10-19 16:35:00 +0545

    GRUB customization

---
+++ scratch.sh
@@ -0,0 +1,1 @@
+rsync -avzPh --exclude "/home/logan/Downloads/copied from big usb/" --exclude /home/logan/blender-files/ --exclude /home/logan/miscallaneous_knowledge/wikipedia_en_all_maxi_2023-09.zim --exclude /home/logan/.local/share/Steam/ --exclude /home/logan/Videos/ --exclude /home/logan/.cache/ --exclude /home/logan/.var/app/com.spotify.Client/ /home/logan/ pc:/home/logan/laptop-backup/
