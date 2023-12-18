8eb6c1bd93c89522d8fb0a5a239414b1e1fb155a
Logan Hillyer <shivastranded@proton.me>
2023-12-18 22:41:37 +0100

    venv not working

--- fedora-stable-diffusion-download.sh
+++ fedora-stable-diffusion-download.sh
@@ -5,8 +5,9 @@ cd ~
 wget -q https://raw.githubusercontent.com/AUTOMATIC1111/stable-diffusion-webui/master/webui.sh
 chmod +x ./webui.sh
 # this should fail with a bad python version
-./webui.sh || (sudo dnf install python3.10 && cd stable-diffusion-webui &&\
+./webui.sh || (echo "script failed" && sudo dnf install python3.10 && cd stable-diffusion-webui &&\
 python3.10 -m venv venv && which python && source venv/bin/activate &&\
  pip install --upgrade pip && which python && ./webui.sh)
-
+echo "stable diffusion installed"
 sh <(curl -sL https://raw.githubusercontent.com/12to-the12/spark/main/install-models.sh)
+sh <(curl -sL https://raw.githubusercontent.com/12to-the12/spark/main/fedora-stable-diffusion-download.sh)
