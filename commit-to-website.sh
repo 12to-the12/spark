9a96c71e97d96d927edeacdc96f52033a11d1601
Logan Hillyer <shivastranded@proton.me>
2024-02-03 14:02:23 +0000

    added stable diffusion scripts

--- commit-to-website.sh
+++ commit-to-website.sh
@@ -5,6 +5,9 @@ git add . && git commit -m "$1" && git push origin main
 
 rsync -aPh ./spark $WEBDEV_PROJECTS_DIR/personal-website/dist/spark
 rsync -aPh ./fedora $WEBDEV_PROJECTS_DIR/personal-website/dist/fedora
+rsync -aPh ./sd-fedora $WEBDEV_PROJECTS_DIR/personal-website/dist/sd-fedora
+rsync -aPh ./sd-mac $WEBDEV_PROJECTS_DIR/personal-website/dist/sd-mac
+
 
 
 cd $WEBDEV_PROJECTS_DIR/personal-website/
