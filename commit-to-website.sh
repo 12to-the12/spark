b856c26177667b0e587c6649c60ed64498fe422c
Logan Hillyer <shivastranded@proton.me>
2024-02-03 13:58:29 +0000

    fedora too

--- commit-to-website.sh
+++ commit-to-website.sh
@@ -4,6 +4,8 @@ cd $PROJECTS_DIR/spark/
 git add . && git commit -m "$1" && git push origin main
 
 rsync -aPh ./spark $WEBDEV_PROJECTS_DIR/personal-website/dist/spark
+rsync -aPh ./fedora $WEBDEV_PROJECTS_DIR/personal-website/dist/fedora
+
 
 cd $WEBDEV_PROJECTS_DIR/personal-website/
 git add . && git commit -m "$1" && git push origin main
