b9cbee3d99b0648ef28593b0bd5b51e437557a16
Logan Hillyer <shivastranded@proton.me>
2024-02-03 13:55:48 +0000

    attempt

--- commit-to-website.sh
+++ commit-to-website.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 cd $PROJECTS_DIR/spark/
-git add ./* && git commit -m "$1" && git push
+git add . && git commit -m "$1" && git push origin main
 
 rsync -aPh ./spark $WEBDEV_PROJECTS_DIR/personal-website/dist/spark
 
 cd $WEBDEV_PROJECTS_DIR/personal-website/
-git add ./* && git commit -m "$1" && git push
+git add . && git commit -m "$1" && git push origin main
